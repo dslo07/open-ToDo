6539dbe5b8f0f01ea0f353b5ddb72ed841e1d03f
Santi_DMC <eldwigth25@gmail.com>
2025-08-14 11:05:50 -0400

    solucion cardTask con los botones y restructura de mainContent

--- openToDo/src/components/CardTask.jsx
+++ openToDo/src/components/CardTask.jsx
@@ -20,20 +20,20 @@ const   CardTask = ({ task }) => {
 
     //setPriority(task.priority)
     return (
-    <div id={task.id} className='bg-gray-900 p-3 rounded-lg border-[1px] h-[170px] w-[330px] border-gray-500 transition-transform transform hover:scale-102 hover:shadow-lg ' >
+    <div id={task.id} className='bg-gray-900 p-3 flex flex-col justify-between rounded-lg border-[1px] h-[170px] w-[330px] border-gray-500 transition-transform transform hover:scale-102 hover:shadow-lg ' >
       <div>
-        <div className='flex justify-between '>
-            <span className={setPriority(task.priority)}>{task.priority}</span>
-            <span className='text-sm border-[1px] py-1 px-2 rounded border-gray-400 bg-gray-600'>{task.expiration === "" ? "No-Expiration" : task.expiration  }</span>
-        </div>
-        <div >
-            <h2 className='text-center font-semibold my-1 text-1xl '>{task.name}</h2>
-            <p className='pb-2 leading-[17px] text-sm text-gray-400'>{task.description}</p>
-        </div>
+          <div className='flex justify-between '>
+              <span className={setPriority(task.priority)}>{task.priority}</span>
+              <span className='text-sm border-[1px] py-1 px-2 rounded border-gray-400 bg-gray-600'>{task.expiration === "" ? "No-Expiration" : task.expiration  }</span>
+          </div>
+          <div >
+              <h2 className='text-center font-semibold my-1 text-1xl '>{task.name}</h2>
+              <p className='pb-2 leading-[17px] text-sm text-gray-400'>{task.description}</p>
+          </div>
       </div>
-      <div className='flex  gap-2 '>
-        <button className="border w-70 justify-center font-semibold py-1 px-2 rounded cursor-pointer text-sm flex items-center hover:bg-gray-100 hover:text-gray-900">Ready</button>
-        <button className="border  font-semibold py-1 px-2 rounded cursor-pointer text-sm flex items-center hover:bg-gray-100 hover:text-gray-900">Edit</button>
+      <div className='flex mt-full gap-2 '>
+          <button className="border w-70 justify-center font-semibold py-1 px-2 rounded cursor-pointer text-sm flex items-center hover:bg-gray-100 hover:text-gray-900">Ready</button>
+          <button className="border  font-semibold py-1 px-2 rounded cursor-pointer text-sm flex items-center hover:bg-gray-100 hover:text-gray-900">Edit</button>
       </div>
     </div>
   )
--- openToDo/src/components/MainContent.jsx
+++ openToDo/src/components/MainContent.jsx
@@ -22,8 +22,8 @@ const MainContent = ({ userData }) => {
         <div className="rounded border  h-[305px] border-gray-500 bg-gradient-to-tl from-gray-800 to-gray-700">
           <div className="flex justify-end px-4 py-2">
             <button className="flex items-center p-1 text-xl rounded cursor-pointer transition-transform transform hover:scale-110 hover:shadow-lg " onClick={() => setOpen(true)}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
               </svg>
             </button>
           </div>
@@ -47,21 +47,15 @@ const MainContent = ({ userData }) => {
           </select>
           <input type="date" name="" id=""  className="px-3 rounded bg-gray-900 text-white placeholder-gray-400" />
           <div className='flex justify-end gap-2'>
-            <button>
-              <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0">
-                <span className="sr-only">Previous</span>
-                <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5">
-                  <path d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" fill-rule="evenodd" />
-                </svg>
-              </a>
+            <button type="button" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0" aria-label="Previous">
+              <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="size-5" >
+                <path d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clipRule="evenodd" fillRule="evenodd" />
+              </svg>
             </button>
-            <button>
-                <a href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0">
-                  <span className="sr-only">Next</span>
-                  <svg viewBox="0 0 20 20" fill="currentColor" data-slot="icon" aria-hidden="true" className="size-5">
-                    <path d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
-                  </svg>
-                </a>
+            <button type="button" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 inset-ring inset-ring-gray-700 hover:bg-white/5 focus:z-20 focus:outline-offset-0" aria-label="Next" >
+              <svg viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" className="size-5"  >
+                <path d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" fillRule="evenodd" />
+              </svg>
             </button>
           </div>
         </div>
