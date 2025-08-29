8a78b4ad60d8035bb7a45c5303d05b05edd5f612
Santi_DMC <eldwigth25@gmail.com>
2025-08-29 09:00:12 -0400

    Se integra Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import animations from '@midudev/tailwind-animations'
-// No necesitas tailwindcss como plugin en Vite.
-// Tailwind funciona solo con el archivo postcss.config.js y tailwind.config.js
 
 export default defineConfig({
-  plugins: [react(),tailwindcss(),animations ],
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
+  plugins: [react(),tailwindcss() ]
 })
